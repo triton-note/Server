1dde52cdbfc574caabd68459a3ef293b985f5683
Kunio SAWATANI <k.fathens@gmail.com>
2013-04-13 15:33:54 +0900

    Create tables

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -7,6 +7,13 @@ import scala.slick.driver.PostgresDriver.simple._
 
 object DB {
   def now = new java.sql.Date(new java.util.Date().getTime)
-  val db = Database.forDataSource(PlayDB.getDataSource())
+  lazy val db = Database.forDataSource(PlayDB.getDataSource())
   def withSession[T](f: Session => T) = db.withSession(f)
+  val prepared = {
+    withSession { implicit session: Session =>
+      (User.ddl ++ Photo.ddl ++ Album.ddl).create
+      (PhotoAlbum.ddl ++ PhotoOwner.ddl ++ AlbumOwner.ddl).create
+      (Comment.ddl ++ PhotoComment.ddl ++ AlbumComment.ddl).create
+    }
+  }
 }
