17f53c2a130d738ea2aaa1ad1174d59590807ed8
Kunio SAWATANI <k.fathens@gmail.com>
2013-04-20 22:28:46 +0900

    Add SecureSocial Plugin

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "0.1.0-SNAPSHOT"
 
   val appDependencies = Seq(
+    "securesocial" %% "securesocial" % "master-SNAPSHOT",
     "com.amazonaws" % "aws-java-sdk" % "1.4.1",
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
     "com.typesafe.slick" %% "slick" % "1.0.0",
@@ -17,7 +18,7 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
   )
 
 }
