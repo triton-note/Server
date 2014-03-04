98c04c4154cd6f8101857d2da75775dce49f850c
Kunio SAWATANI <devel.fathens@gmail.com>
2014-03-04 22:25:33 +0900

    Change AWS env name

--- app/service/AWS.scala
+++ app/service/AWS.scala
@@ -3,7 +3,7 @@ package service
 import com.amazonaws.auth.BasicAWSCredentials
 
 object AWS {
-  lazy val endpoint = System.getenv("AWS_ENDPOINT")
+  lazy val region = System.getenv("AWS_REGION")
   lazy val credential = {
     val id = System.getenv("AWS_ACCESS_KEY_ID")
     val key = System.getenv("AWS_SECRET_ACCESS_KEY")
@@ -12,15 +12,16 @@ object AWS {
   object DynamoDB {
     lazy val client = {
       val c = new com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient(credential)
-      c.setEndpoint(endpoint)
+      c.setEndpoint(f"dynamodb.${region}.amazonaws.com")
       c
     }
   }
   object S3 {
-    lazy val bucketName = System.getenv("S3_BUCKET_NAME")
+    lazy val bucketName = System.getenv("AWS_S3_BUCKET_NAME")
     lazy val client = {
       val c = new com.amazonaws.services.s3.AmazonS3Client(credential)
-      c.setEndpoint(endpoint)
+      val ep = if (region == "us-east-1") "" else f"-$region"
+      c.setEndpoint(f"s3${ep}.amazonaws.com")
       c
     }
   }
