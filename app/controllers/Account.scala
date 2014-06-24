d644f8f9fc4caed3294e501cc1a553432cdcea67
Kunio SAWATANI <devel@fathens.org>
2014-06-24 18:33:53 +0900

    text ではなく json で統一するようにした

--- app/controllers/Account.scala
+++ app/controllers/Account.scala
@@ -1,22 +1,24 @@
 package controllers
 
-import scala.annotation.{implicitNotFound, tailrec}
+import scala.annotation.{ implicitNotFound, tailrec }
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 import play.api.Logger
 import play.api.libs.json._
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{ Action, Controller }
 
-import models.{Facebook, Settings}
-import models.db.{Users, VolatileTokens}
+import models.{ Facebook, Settings }
+import models.db.{ Users, VolatileTokens }
 
 object Account extends Controller {
   def auth(way: String, token: String) = way match {
     case "facebook" => Facebook.User(token)
     case _          => Future(None)
   }
-  def login(way: String) = Action.async(parse.text) { implicit request =>
+  def login(way: String) = Action.async(parse.json(
+    (__ \ "token").read[String]
+  )) { implicit request =>
     val token = request.body
     auth(way, token).map { u =>
       Logger debug f"Authorized user from $way: $u"
