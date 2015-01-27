537ca38f3d7604dc2db5f630b99d9d783a43a6b5
Kunio Sawatani <devel@fathens.org>
2015-01-27 15:27:38 +0900

    月の位置から潮汐を推定する実装

---
+++ app/service/TideMoon.scala
@@ -0,0 +1,30 @@
+package service
+
+import org.fathens.astronomy.Moon
+import org.fathens.math._
+
+import models.GeoInfo
+
+object TideMoon {
+  object TideState extends Enumeration {
+    val High = Value("High")
+    val Ebb = Value("Ebb")
+    val Low = Value("Low")
+    val Flood = Value("Flood")
+  }
+}
+class TideMoon(val date: java.util.Date, val geoinfo: GeoInfo) {
+  import TideMoon._
+
+  val moon = new Moon(date)
+
+  val state = {
+    val diff: Degrees = (moon.earth_longitude - geoinfo.longitude + Degrees(15)).normalize
+    (diff % Pi).value match {
+      case d if d < 30             => TideState.High
+      case d if 30 <= d && d <= 90 => TideState.Ebb
+      case d if 90 < d && d < 120  => TideState.Low
+      case d if 120 <= d           => TideState.Flood
+    }
+  }
+}
