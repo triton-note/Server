bc734cb867d49b5c7bef5fa660c0b1b14e202534
Kunio Sawatani <devel@fathens.org>
2015-01-28 17:46:44 +0900

    GeoInfo の Json Format を変更
    Degrees のフォーマットを間接的に指定したのではエラーになったので修正します。

--- app/models/GeoInfo.scala
+++ app/models/GeoInfo.scala
@@ -11,6 +11,11 @@ import org.fathens.math._
  */
 case class GeoInfo(latitude: Degrees, longitude: Degrees)
 object GeoInfo {
-  implicit val degreesFormat: Format[Degrees] = __.format[Double].inmap(Degrees.apply, unlift(Degrees.unapply))
-  implicit val geoinfoFormat = Json.format[GeoInfo]
+  implicit val geoinfoFormat: Format[GeoInfo] = {
+    def degrees(p: JsPath) = p.format[Double].inmap(Degrees.apply, unlift(Degrees.unapply))
+    (
+      degrees(__ \ "latitude") and
+      degrees(__ \ "longitude")
+    )(GeoInfo.apply, unlift(GeoInfo.unapply))
+  }
 }
