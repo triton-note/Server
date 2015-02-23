9e990dc9515b9e3a9bf72c3a8eb7f776bf96b5f1
Kunio Sawatani <devel@fathens.org>
2015-02-23 23:00:28 +0900

    変数名変更の修正し忘れ

--- app/models/Distributions.scala
+++ app/models/Distributions.scala
@@ -33,8 +33,8 @@ object Distributions {
       fish <- report.fishes
     } yield Catch(
       userOption.map(_ => report.id),
-      fish.name,
-      fish.count,
+      fish.nominal,
+      fish.quantity,
       report.dateAt,
       report.location.geoinfo)
   }
