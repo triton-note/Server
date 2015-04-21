7ff42e0cae7420a4fcbbec76510aad0c20f253f4
Kunio Sawatani <devel@fathens.org>
2015-04-21 21:53:51 +0900

    ブラウザからもアクセスできるように CORS を設定

--- app/Global.scala
+++ app/Global.scala
@@ -8,7 +8,7 @@ import play.api.mvc.{ Filter, RequestHeader, Result, WithFilters }
 
 import models.VolatileToken
 
-object Global extends WithFilters(AccessLog) {
+object Global extends WithFilters(AccessLog, CrossOriginResource) {
   override def onStart(app: Application) {
     Akka.system(app).scheduler.schedule(0 seconds, 1 hours) {
       val count = VolatileToken.deleteExpired
@@ -24,5 +24,19 @@ object AccessLog extends Filter {
       Logger debug f"${request} headers: ${request.headers.toSimpleMap} \n\t =>  ${r}(status=${r.header.status}) headers: ${r.header.headers}"
     }
     result
+  }
+}
+
+object CrossOriginResource extends Filter {
+  override def apply(next: RequestHeader => Future[Result])(request: RequestHeader): Future[Result] = {
+    val isPreFlight =
+      request.method.toUpperCase.equals("OPTIONS") && request.headers.get("Access-Control-Request-Method").nonEmpty
+    def addHeaders(r: Result) = r.withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "POST, GET",
+      "Access-Control-Allow-Headers" -> "Content-Type"
+    )
+    if (isPreFlight) Future { addHeaders(controllers.Default.Ok) }
+    else next(request).map(addHeaders)
   }
 }
